729dd6d144be697fed2a92c892c6d50140367fea
siwka <ms.ksiwek@gmail.com>
2015-01-29 10:11:01 -0600

    loading csv, assign headers to hash keys

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -6,7 +6,12 @@ class Auction < ActiveRecord::Base
 
 	def self.import(file)
 	  CSV.foreach(file.path, headers: true) do |row|
-	    Auction.create! row.to_hash
+	    Auction.create!(name: row[0],
+	                    street: row[1],
+	                    city: row[2],
+	                    state: row[3],
+	                    zip: row[4])
+	    # auction.vehicle.create!
 	  end
 	end                    
 end
